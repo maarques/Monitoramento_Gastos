2244e6cf8e44642d5fc0548fabbb5817908dbbf4
maarques <thi4go.marqu3s@gmail.com>
2025-10-01 14:12:24 -0300

    acessando a aplicação sem uma hospedagem

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,10 @@ import os
 def create_app():
     app = Flask(__name__, template_folder="../templates", static_folder="../static")
     app.config['UPLOAD_FOLDER'] = str(Path.cwd() / "data")
-    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+    from . import routes
+    app.register_blueprint(routes.bp)
+
     return app
--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from app import create_app
-from app.routes import bp as main_bp
 
 app = create_app()
-app.register_blueprint(main_bp)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
