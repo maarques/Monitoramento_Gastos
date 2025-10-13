e8fc2a941dbc3b3605892cd4d08e472e6ecef36b
maarques <thi4go.marqu3s@gmail.com>
2025-10-13 14:13:03 -0300

    criando uma maneira mais fácil de acessar, como um executável

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+import webbrowser
+from threading import Timer
 from app.app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+def open_browser():
+    webbrowser.open_new("http://127.0.0.1:5000/")
+
+if __name__ == "__main__":
+    Timer(1, open_browser).start()
+    app.run(host="127.0.0.1", port=5000, debug=True)
